823782652b5029fe214a0530c7eb8cdeffb729ff
Sandeep-morya <saabmaurya@gmail.com>
2023-04-25 13:40:45 +0530

    added a loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 ﻿import { MantineProvider, Text, Button, Stack } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import Boot from "./components/Misc/Boot";
 import AllRoutes from "./Routes";
 
 export default function App() {
-	return (
+	const [loading, setLoading] = useState(true);
+
+	const heatUpTheServer = useCallback(async () => {
+		await axios.get(import.meta.env.VITE_API_URL);
+		setLoading(false);
+	}, []);
+
+	useEffect(() => {
+		heatUpTheServer();
+	}, [heatUpTheServer]);
+	return loading ? (
+		<Boot />
+	) : (
 		<MantineProvider
 			withGlobalStyles
 			withNormalizeCSS
---
+++ src/components/Misc/Boot.tsx
@@ -0,0 +1,33 @@
+﻿import { Image, Loader } from "@mantine/core";
+
+const Boot = () => {
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				display: "grid",
+				placeItems: "center",
+				overflow: "hidden",
+			}}>
+			<Image
+				width={"7rem"}
+				height={"7rem"}
+				src={
+					"https://res.cloudinary.com/due9pi68z/image/upload/v1680333681/ntpzmiiotgzmn9loafur.png"
+				}
+				alt="logo"
+			/>
+			<Loader
+				style={{
+					position: "absolute",
+					bottom: "5%",
+					left: "50%",
+					transform: "translateX(-50%)",
+				}}
+			/>
+		</div>
+	);
+};
+
+export default Boot;
